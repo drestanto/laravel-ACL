1c8c25a95a4f5dccd2f6b723338560d3aeceeb3a
Dyas <dyas@live.com>
2017-06-05 11:36:40 +0700

    create and setting editorMiddleware

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -14,6 +14,7 @@ class EditorController extends Controller
     public function __construct()
     {
         $this->middleware('auth:admin');
+        $this->middleware('editor');
     }
 
     public function index()
