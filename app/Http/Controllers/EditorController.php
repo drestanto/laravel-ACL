c3948dca251055cf7a14639fcfa6a5c7e1bfc7e0
Dyas <dyas@live.com>
2017-06-05 11:30:06 +0700

    admin and editor manage

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -6,6 +6,16 @@ use Illuminate\Http\Request;
 
 class EditorController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
     public function index()
     {
     	return view('admin.editor');
